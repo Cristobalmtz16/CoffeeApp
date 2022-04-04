2ae3a09030d79e5474ace02c15a4210e01fd9853
miramonster <95644636+miramonster@users.noreply.github.com>
2022-04-03 23:42:49 -0500

    updating css

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
 import "./App.css";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter, Route, Routes,} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 //import Navbar from "./components/Navbar";
 import Homescreen from "./screens/Homescreen";
 import Cartscreen from "./screens/Cartscreen";
 import Landingscreen from "./screens/Landingscreen";
-import Header from './components/Header';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
 
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landingscreen />} exact />
-          <Route path="/shop" element={<Homescreen />} exact />
-          <Route path="/cart" element={<Cartscreen />} exact />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landingscreen />} exact />
+        <Route path="/shop" element={<Homescreen />} exact />
+        <Route path="/cart" element={<Cartscreen />} exact />
+      </Routes>
     </div>
   );
 }
--- client/src/screens/Landingscreen.js
+++ client/src/screens/Landingscreen.js
@@ -1,11 +1,21 @@
 import React from "react";
-import lobbyPic from './coffeeLobby2.jpg';
-import './Landingscreen.css'
+import lobbyPic from "./coffeeLobby2.jpg";
+import "./Landingscreen.css";
 
 const Landingscreen = () => {
-
-  return <img src={lobbyPic} alt="Company logo"/>
+  return (
+    <div style={{ minWidth: "100vw" }}>
+      <div id="heroContainer">
+        <img className="lobbyImg" src={lobbyPic} alt="Company logo" />
+      </div>
+      <div className="footer ">
+     
+        
+        <img src="instagramLogo.png" className="instagramLink" to="https://instagram.com/three_bayous_beanery" />
+      
+      </div>
+    </div>
+  );
 };
-
 
 export default Landingscreen;
